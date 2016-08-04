9b75fcbec8660cc4273cfeb4393f573d76936d15
egrils <egrils@users.noreply.github.com>
2016-08-04 09:46:12 +0800

    add app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-// TODO
+const App = React.createClass({
+    render: function () {
+        return <div>
+            <button>Preview</button>
+            <Editor />
+            <Previewer />
+            </div>
+    }
+});
+
+const Editor = React.createClass({
+    render: function () {
+        return <div>
+                Editor
+            </div>
+    }
+});
+
+const Previewer = React.createClass({
+    render: function () {
+        return <div>
+                Previewer
+            </div>
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('content'));
